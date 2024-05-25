99910cd83a41479541f377d6926a75f68a362a9a
JJgotBored <jasonwbere@gmail.com>
2024-05-25 16:12:30 -0400

    updated Griddify.py
    - added template for widget from krita documentation

--- Griddify.py
+++ Griddify.py
@@ -1,3 +1,15 @@
 from PyQt5.QtWidgets import *
-import krita
+#import krita
+from krita import *
+
+class Griddify(DockWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Griddify")
+
+    def canvasChanged(self, canvas):
+        pass
+
+Krita.instance().addDockWidgetFactory(DockWidgetFactory("griddify", DockWidgetFactoryBase.DockRight, Griddify))
 
